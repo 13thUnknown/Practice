80a674f3ccba94633ec520b7049d01577ec12beb
Alex <alexsoldat98@gmail.com>
2018-07-16 07:29:19 +0300

    доработал запись

--- Practice/src/FileWork.java
+++ Practice/src/FileWork.java
@@ -11,6 +11,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+import java.sql.Time;
 import java.util.*;
 
 public class FileWork
@@ -30,10 +31,17 @@ public class FileWork
             n++;
             String nameSheet = String.valueOf(n);
             Sheet sheet = book.createSheet(nameSheet);
+            int numstr = 1;
+            Row row = sheet.createRow(0);
+            Cell TimeList = row.createCell(0);
+            TimeList.setCellValue(5000);
+            Cell NumList = row.createCell(1);
+            NumList.setCellValue(n);
+            Cell ValueList = row.createCell(2);
+            ValueList.setCellValue(List.getTableSize());
             while (element != null)
             {
-                int numstr = 0;
-                Row row = sheet.createRow(numstr);
+                row = sheet.createRow(numstr);
                 Cell CellChatID = row.createCell(0);
                 CellChatID.setCellValue(element.getChatID());
                 Cell CellNumber = row.createCell(1);
@@ -47,8 +55,8 @@ public class FileWork
         book.close();
     }
 
-    public void  ReadFromExcel (Info ListIn) throws Exception {
-        List = ListIn;
+    public void  ReadFromExcel () throws Exception {
+        List = null;
         element = List;
         HSSFWorkbook ExcelBook = new HSSFWorkbook(new FileInputStream("file"));  // вместо file должен быть путь к фалу
         Iterator<Sheet> sheets = ExcelBook.sheetIterator();
@@ -56,13 +64,26 @@ public class FileWork
         {
             HSSFSheet ExcelSheet = (HSSFSheet) sheets.next();
             Iterator<Row> rows = ExcelSheet.rowIterator();
+            HSSFRow row = (HSSFRow) rows.next();
+            HSSFCell TimeList = row.getCell(0);
+            HSSFCell NumList = row.getCell(1);
+            HSSFCell ValueList = row.getCell(2);
+            List = new List(Integer.getInteger(TimeList.toString(),Integer.getInteger(NumList.toString(),Integer.getInteger(ValueList.toString())));
+            element = List;
             while (rows.hasNext())
             {
-                HSSFRow row = (HSSFRow) rows.next();
+                row = (HSSFRow) rows.next();
                 HSSFCell ChatID = row.getCell(0);
                 HSSFCell Number = row.getCell(1);
                 if (ChatID != null && Number != null) {
-                    //добавление в очередь
+//                    if (element == List) {
+//                        element.setChatID(Integer.getInteger(ChatID.toString()));
+//                        element.setNumber(Integer.getInteger(Number.toString()));
+//                    }
+//                    else {
+//                        element.setNext(new Info());
+//                        element = element.getNext()
+//                    }
                 }
 
             }
